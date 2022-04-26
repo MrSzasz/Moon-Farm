f837357a6412aa3c14b1231c19a21ffb55e21de7
MrSzasz <lugoleandro@hotmail.com>
2022-04-26 13:45:31 -0300

    header component donne

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+function NavBar() {
+  return (
+    <header>
+      <div className="logo text-center"> MOON FARM 🌙</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">INICIO</a>
+          </li>
+          <li>
+            <a href="#">TIENDA</a>
+          </li>
+          <li>
+            <a href="#">CARRITO</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
