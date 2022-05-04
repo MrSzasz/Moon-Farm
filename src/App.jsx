2fbf375c4e4b67f7de027bf6651a15cfd24eed23
MrSzasz <lugoleandro@hotmail.com>
2022-05-04 15:17:12 -0300

    counter: done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,60 @@
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar.jsx";
+import ItemCount from "./components/ItemCount/ItemCount";
+
 
 import "./App.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const greeting = "Hola! Soy un greeting!";
 
-  const greeting = 'Hola! Soy un greeting!';
-  
   return (
-
-  <>
-    <NavBar />
-    <ItemListContainer greeting={greeting} subGreeting='Bienvenido a mi pagina!' />
-  </>
-  
-  )
+    <>
+      <NavBar />
+      <ItemListContainer
+        greeting={greeting}
+        subGreeting="Bienvenido a mi pagina!"
+      />
+      <ItemCount stock={7} initial={1} />
+    </>
+  );
 }
 
 export default App;
+
+// import React, { useState, useEffect } from 'react';
+// import './style.css';
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [date, SetDate] = useState(Date);
+// const [boolean, setBoolean] = useState(false)
+
+//   function addCount() {
+//     setCount(count + 1);
+//     SetDate(Date())
+//   }
+
+// useEffect(() => { // se usa para procesarse LUEGO del render
+//   console.log('hola')
+// }, []) // un solo render
+
+// useEffect(() => { // se usa para procesarse LUEGO del render
+//   console.log("solo cuando cambia el boolean")
+// }, [boolean]) // dato a watchear
+
+// function changeBoolean(){
+// setBoolean(!boolean);
+// }
+
+//   return (
+//     <div>
+//       <button onClick={addCount}>Valor: {count}</button>
+//       <button onClick={changeBoolean}>Bool</button>
+//       <p>Hoy es {date}</p>
+//     </div>
+//   );
+// }
+
+// export default App
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { ImMinus, ImPlus } from "react-icons/im";
+import "./ItemCount.css";
+
+function ItemCount({ stock, initial, onAdd }) {
+  const [counter, setCounter] = useState(initial);
+
+  function addCounter() {
+    if (counter < stock) {
+      setCounter(counter + 1);
+    }
+  }
+
+  function removeCounter() {
+    if (counter > initial) {
+      setCounter(counter - 1);
+    }
+  }
+
+  function onAdd() {
+    console.log(`Agregaste ${counter} objeto/s al carrito`);
+  }
+
+  return (
+    <div className="counterContainer">
+      <h2>Stock disponible = {stock}</h2>
+      <div className="counterControl">
+        <button className="btn btn-danger" onClick={removeCounter}><ImMinus/></button>
+        <p className="counterStatus">{counter}</p>
+        <button className="btn btn-success" onClick={addCounter}><ImPlus/></button>
+      </div>
+      <button className="btn btn-primary"onClick={onAdd}>Agregar al carrito</button>
+    </div>
+  );
+}
+
+export default ItemCount;
