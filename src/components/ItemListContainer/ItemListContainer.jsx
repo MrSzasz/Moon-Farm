dfd37b946e7996812fc3643fda6d2a4d3b55caec
MrSzasz <lugoleandro@hotmail.com>
2022-06-01 20:18:56 -0300

    item list container working, pre branch

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,44 +2,65 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import ItemList from "../ItemList/ItemList";
 import "./ItemListContainer.scss";
-import { getFetch } from "../../helpers/getFetch";
 import { useNightContext } from "../../context/NightContext/NightContext";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
 const ItemListContainer = () => {
-  const [packsFromList, setPacksFromList] = useState([]);
   const [filteredList, setFilteredList] = useState([]);
   const [loading, setLoading] = useState(true);
   const { seasonUrl } = useParams();
 
-const {isNight} = useNightContext();
+  const { isNight } = useNightContext();
 
   useEffect(() => {
-    getFetch()
-      .then((res) => setPacksFromList(res))
+    const db = getFirestore();
+
+    const queryCollection = collection(db, "products");
+
+    const queryCollectionFiltered = query(
+      queryCollection,
+      where("season", "==", seasonUrl)
+    );
+
+    getDocs(queryCollectionFiltered)
+      .then((res) =>
+        setFilteredList(
+          res.docs.map((item) => ({ ...item.data(), id: item.id }))
+        )
+      )
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   }, []);
 
-  useEffect(() => {
-    setFilteredList(
-      packsFromList.filter((seasonPack) => seasonPack.season == seasonUrl)
-    );
-  });
-
   return (
-    <div className={`d-flex flex-wrap w-100 justify-content-center align-items-center containerPacks ${isNight?'nightContainer':""}`}>
+    <div
+      className={`d-flex flex-wrap w-100 justify-content-center align-items-center containerPacks ${
+        isNight ? "nightContainer" : ""
+      }`}
+    >
       {loading ? (
-        <div className={`text-center loadingDiv ${isNight?'loadingNight':""}`}>
-        <span className="loader">
-          <span className="loader-inner"></span>
-        </span>
-        <h2 className="mt-4">LOADING...</h2>
-      </div>
+        <div
+          className={`text-center loadingDiv ${isNight ? "loadingNight" : ""}`}
+        >
+          <span className="loader">
+            <span className="loader-inner"></span>
+          </span>
+          <h2 className="mt-4">LOADING...</h2>
+        </div>
       ) : (
         <div className="mainPacks">
           <h2>{seasonUrl.toUpperCase()}</h2>
-          <ItemList packs={filteredList}/>
-          <Link className={`btn mainButton ${isNight?'mainButtonNight':""}`}  to="/tienda">
+          <ItemList packs={filteredList} />
+          <Link
+            className={`btn mainButton ${isNight ? "mainButtonNight" : ""}`}
+            to="/tienda"
+          >
             &lt; VOLVER
           </Link>
         </div>
