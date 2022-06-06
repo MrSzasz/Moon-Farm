62c50a871074fdefb9ebaab0374f1f2abfdfc33e
MrSzasz <lugoleandro@hotmail.com>
2022-06-06 15:17:44 -0300

    logo changed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,26 @@
 import CartWidget from "../CartWidget/CartWidget";
-import { BsFillMoonFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
 import { useNightContext } from "../../context/NightContext/NightContext";
 
 function NavBar() {
-
-  const {isNight} = useNightContext();
+  const { isNight } = useNightContext();
 
   return (
     <header className={isNight}>
-        <Link to="/" className="logo text-center d-flex justify-content-center align-items-center gap-2 linkOnNavBar">
-          MOON FARM <BsFillMoonFill className="iconFont" />
-        </Link>
+      <Link
+        to="/"
+        className="logo text-center d-flex justify-content-center align-items-center gap-2 linkOnNavBar"
+      >
+        <img src="https://i.imgur.com/Kh3WU4k.png" alt="" />
+      </Link>
       <nav>
         <ul>
           <Link to="/" className="linkOnNavBar">
-            <li>INICIO</li>
+            <li className="liWithHover">INICIO</li>
           </Link>
           <Link to="/tienda" className="linkOnNavBar">
-            <li>TIENDA</li>
+            <li className="liWithHover">TIENDA</li>
           </Link>
           <CartWidget />
         </ul>
